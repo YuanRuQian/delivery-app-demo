113c2754e43adeadd31ee0d9b9c9f7676885c796
Lydia Yuan <lydiayuan99@gmail.com>
2024-02-20 10:12:45 -0500

    add: sub to a topic

--- app/src/main/java/lydia/yuan/deliveryappdemo/network/MyFirebaseMessagingService.kt
+++ app/src/main/java/lydia/yuan/deliveryappdemo/network/MyFirebaseMessagingService.kt
@@ -1,10 +1,7 @@
 package lydia.yuan.deliveryappdemo.network
 
-import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import android.media.RingtoneManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -12,7 +9,6 @@ import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import lydia.yuan.deliveryappdemo.MainActivity
 import lydia.yuan.deliveryappdemo.R
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
